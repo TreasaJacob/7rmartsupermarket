2a911384673a4fcad7af19c06eb4298876b6f628
treas <treas@Stevu>
2025-04-03 15:33:39 +0530

    Screenshot capturing code in Base class and Screenshot utility

--- src/test/java/com/obsqura/rmart_Supermarket/Testscript/Base.java
+++ src/test/java/com/obsqura/rmart_Supermarket/Testscript/Base.java
@@ -1,12 +1,17 @@
 package com.obsqura.rmart_Supermarket.Testscript;
 
 import org.testng.annotations.Test;
+
+import com.obsqura.rmat_utilities.ScreenshotUtility;
+
 import org.testng.annotations.BeforeMethod;
 
+import java.io.IOException;
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 
 public class Base {
@@ -21,7 +26,15 @@ public class Base {
   }
 
   @AfterMethod
-  public void afterMethod() {
+  public void afterMethod(ITestResult itResult) throws IOException {
+	  if (itResult.getStatus() == ITestResult.FAILURE) {  //ITestResult is an interface //getStatus() is a method in ITestResult interface.
+			ScreenshotUtility sc = new ScreenshotUtility();  //name of created java class
+			sc.captureFailureScreenShot(driver, itResult.getName());//getName()-to capture name of failed test case
+		}
+		if (driver != null) {
+			driver.quit();
+		}
+	  
   }
 
 }
